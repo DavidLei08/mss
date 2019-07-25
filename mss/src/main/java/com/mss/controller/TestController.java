88fc7d9152773b1a4f0437d40e236a68397b604d
DavidLei08 <you@example.com>
2019-07-26 00:22:10 +0800

    first commit

---
+++ mss/src/main/java/com/mss/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.mss.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/test")
+@Controller
+public class TestController {
+
+	@RequestMapping("/")
+	public String test(){
+		return "test/test";
+	}
+}
